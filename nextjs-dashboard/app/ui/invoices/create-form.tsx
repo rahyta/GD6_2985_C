b60578008611dc2ad9df80c1d6c785137b1b2e7f
rahyta <rahytaafrilia@gmail.com>
2026-05-21 17:13:00 +0700

    intial commit

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -1,4 +1,4 @@
-import { CustomerField } from '@/app/lib/definitions';
+import { Customer, CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -7,10 +7,15 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoices } from '@/app/lib/actions';
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default function Form({
+  customers,
+}: {
+  customers: CustomerField[];
+}) {
   return (
-    <form>
+    <form action={createInvoices}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
